01bbadebc3ad6eaed66e93bc0b5773125f81a1d1
Joan García <115421820+joangarciamira@users.noreply.github.com>
2024-03-10 19:29:41 +0100

    testcontainers

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.polarbookshop.catalogservice.domain.Book;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = CatalogServiceApplication.class)
+@ActiveProfiles("integration")
 @AutoConfigureWebTestClient
 public class CatalogServiceApplicationTests {
 
---
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -0,0 +1,41 @@
+package com.polarbookshop.catalogservice.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.polarbookshop.catalogservice.config.DataConfig;
+
+@DataJdbcTest
+@Import(DataConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("integration")
+public class BookRepositoryJdbcTests {
+
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@Autowired
+	private JdbcAggregateTemplate jdbcAggregateTemplate;
+	
+	@Test
+	void findBookByIsbnWhenExisting() {
+		String bookIsbn = "1234567897";
+		Book book = Book.of(bookIsbn, "La hoz de oro", "Goscinny y Uderzo", 10.00);
+		jdbcAggregateTemplate.insert(book);
+		
+		Optional<Book> obtainedBook = bookRepository.findByIsbn(bookIsbn);
+		
+		assertThat(obtainedBook).isPresent();
+		assertThat(obtainedBook.get().isbn()).isEqualTo(bookIsbn);
+		
+	}
+}
